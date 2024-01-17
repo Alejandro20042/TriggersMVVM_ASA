865652a1d90a7fef5d265ab3e5d2e53203585906
Alejandro20042 <alejandrosillasalcantar.com@gmail.com>
2024-01-17 12:18:03 -0700

    Agregar archivos de proyecto.

---
+++ TriggersMVVM_ASA/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using TriggersMVVM_ASA.Vistas;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TriggersMVVM_ASA
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new Categoria();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ TriggersMVVM_ASA/Datos/Dcategorias.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using TriggersMVVM_ASA.VistaModelo;
+
+namespace TriggersMVVM_ASA.Datos
+{
+    public class Dcategorias
+    {
+        public static ObservableCollection<Mcategorias> MostrarCategorias()
+        {
+            return new ObservableCollection<Mcategorias>()
+            {
+                new Mcategorias ()
+                {
+                    descripcion = "Cena",
+                    numeroItem= 4512,
+                    imagen="https://i.ibb.co/nz2jQLh/guino.png",
+                    backgroundColor="#EAEDF6",
+                    textColor="#000000"
+                },
+                new Mcategorias ()
+                {
+                    descripcion = "Hotel",
+                    numeroItem= 4512,
+                    imagen="https://i.ibb.co/vvXnCW9/superestrella.png",
+                    backgroundColor="#EAEDF6",
+                    textColor="#000000"
+                },
+                new Mcategorias ()
+                {
+                    descripcion = "Fiesta",
+                    numeroItem= 4512,
+                    imagen="https://i.ibb.co/5j3sTbn/risa.png",
+                    backgroundColor="#EAEDF6",
+                    textColor="#000000"
+                },
+                new Mcategorias ()
+                {
+                    descripcion = "Flores",
+                    numeroItem= 4512,
+                    imagen="https://i.ibb.co/5j3sTbn/risa.png",
+                    backgroundColor="#EAEDF6",
+                    textColor="#000000"
+                }
+            };
+        }
+    }
+}
