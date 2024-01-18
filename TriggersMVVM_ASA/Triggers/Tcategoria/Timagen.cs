321da63b2514f7a550374d9b993615078094dcb1
Alejandro20042 <alejandrosillasalcantar.com@gmail.com>
2024-01-18 14:27:18 -0700

    Actualizacion 2.1

---
+++ TriggersMVVM_ASA/Triggers/Tcategoria/Timagen.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace TriggersMVVM_ASA.Triggers.Tcategoria
+{
+    public class Timagen : TriggerAction<Image>
+    {
+        public bool activacion {  get; set; }
+        protected override async void Invoke(Image sender)
+        {
+            if(activacion == true)
+            {
+                sender.BackgroundColor = Color.Red;
+                //Rotacion que tendra, tiempo que tardara en milisegundos
+                //tipo de animacion (efecto)
+
+                await sender.RelRotateTo(360, 5000, Easing.BounceOut);
+            }
+            if(activacion == false)
+            {
+                //En esta parte restauramos los valores a un estado normal
+                sender.BackgroundColor = new Image().BackgroundColor;
+                sender.Rotation = new Image().Rotation;
+            }
+        }
+    }
+}
--- TriggersMVVM_ASA/VistaModelo/VMcategoria.cs
+++ TriggersMVVM_ASA/VistaModelo/VMcategoria.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using Xamarin.Forms;
+using Xamarin.Forms.Internals;
 
 namespace TriggersMVVM_ASA.VistaModelo
 {
@@ -12,6 +14,9 @@ namespace TriggersMVVM_ASA.VistaModelo
     {
         #region VARIABLES
         string _Texto;
+        bool _activadorAnimacionImg;
+        string _imagen;
+
         ObservableCollection<Mcategorias> _listaCategorias;
         #endregion
         #region CONSTRUCTOR
@@ -27,6 +32,18 @@ namespace TriggersMVVM_ASA.VistaModelo
             get { return _listaCategorias; }
             set { SetValue(ref _listaCategorias, value); }
         }
+        public bool ActivadorAnimacionImg
+        {
+            get { return _activadorAnimacionImg; }
+            set { SetValue(ref _activadorAnimacionImg, value); }
+        }
+        public string Imagen
+        {
+            get { return _imagen; }
+            set { SetValue(ref _imagen, value); }
+
+        }
+
         #endregion
         #region PROCESOS
         public async Task ProcesoAsyncrono()
@@ -38,10 +55,42 @@ namespace TriggersMVVM_ASA.VistaModelo
             //Al hacer esto estamos jalando toda la data
             ListaCategorias = new ObservableCollection<Mcategorias>(Datos.Dcategorias.MostrarCategorias());
         }
+
+        public void Seleccionar(Mcategorias modelo)
+        {
+            //Con el index identificamos a que imagen le hemos dado click
+            var index = ListaCategorias
+                .ToList()
+                .FindIndex(P => P.descripcion == modelo.descripcion);
+
+            Imagen = modelo.imagen;
+            if(index > -1)
+            {
+                Deseleccionar();
+                ActivadorAnimacionImg = true;
+                ListaCategorias[index].selected = true;
+                ListaCategorias[index].textColor = "#FFFFFF";
+                ListaCategorias[index].backgroundColor = "#FF5068";
+
+            }
+        }
+
+        public void Deseleccionar()
+        {
+            ListaCategorias.ForEach((item) =>
+            {
+                ActivadorAnimacionImg = false;
+                item.selected = false;
+                item.textColor = "#000000";
+                item.backgroundColor = "#EAEDF6";
+
+            });
+        }
         #endregion
         #region COMANDOS
         public ICommand ProsesoAsynCommnad => new Command(async () => await ProcesoAsyncrono());
-        public ICommand MostrarCategoriasCommnad => new Command(MostrarCategorias);
+       // public ICommand MostrarCategoriasCommnad => new Command(MostrarCategorias);
+        public ICommand ProcesoSimpSeleccionar => new Command<Mcategorias>((p) => Seleccionar(p));
         #endregion
     }
 }
